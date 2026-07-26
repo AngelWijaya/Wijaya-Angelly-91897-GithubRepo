905a552b8ba40d5d257a4f69a9dff2bf2039b389
AngelWijaya <24496@my.westlakegirls.school.nz>
2026-07-26 16:49:29 +1200

    fixed deletebtn added scrollbar

--- src/components/TodoList/Todo.tsx
+++ src/components/TodoList/Todo.tsx
@@ -21,7 +21,11 @@ export const TodoList = () =>{
 		setTodos(tmpTodos);
 		setValue("");
 	};
-
+	const deleteTodos = (id: Todo['id']) => {
+		const newTodos = todos.filter(todo => todo.id !== id);
+		setTodos(newTodos);
+		
+	};
 	const inputStyle: React.CSSProperties ={
 		paddingBlock: '1px',
 		paddingInline: '2px',
@@ -52,26 +56,34 @@ export const TodoList = () =>{
 	};
 	const individualTodoStyle: React.CSSProperties={
 		display: 'flex',
+		overflowWrap:'anywhere',
 		alignItems: 'center',
 		margin: '10px 0 0 0',
-		padding: '0px 5px 0 0',
-		maxWidth: '360px',
+		padding: '5px 5px 5px 5px',
+		maxWidth: '350px',
 		height: '40px',
 		backgroundColor: '#f6faff',
 		borderRadius: '10px',
 	};
 	const textTodoStyle:React.CSSProperties ={
+		maxWidth: '340px',
 		width: 'fit-content',
-		maxWidth: '90%',
 		padding: '0 0 0 10px',
 		color: '#68778d',
 		fontWeight: 'bolder',
 	};
 	const todosContainerStyle :React.CSSProperties ={
-		width: '380px',
+		width: '370px',
 		height: '310px',
-		padding: '0 20px 0 20px',
+		maxHeight:'180px',
+		padding: '0px 3px 0 20px',
 		overflowY: 'scroll',
+	};
+	const iconBlockStyle :React.CSSProperties ={
+		background:'none',
+		marginLeft:'auto',
+		position:'sticky',
+		border:'none',
 	};
 	return (
 		<BaseCard
@@ -84,14 +96,20 @@ export const TodoList = () =>{
 		>
 			<div>
 				<div style={todoContentStyle}>
-					<input placeholder="type here.." style={inputStyle} value={value} onChange={(e) => setValue(e.target.value)}></input>
+					<input placeholder="type here.." style={inputStyle} value={value} onChange={(e) => setValue(e.target.value)} maxLength={50}></input>
 					<button style={buttonStyle} onClick={addTodo}>Add</button>
 				</div>
 				<div style={todosContainerStyle}>
-					<div style={individualTodoStyle}>
-						<p style={textTodoStyle}>Todo A</p>
-    					<button>delete</button>
-    				</div>
+					{todos.map((todo) => {
+						return (
+							<div style={individualTodoStyle}>
+								<p style={textTodoStyle}>{todo.label}</p>
+								<button onClick={() => deleteTodos(todo.id)} style={iconBlockStyle}>
+									<img src="icons/check-icon.png" alt="check"/>
+								</button>
+							</div>
+						);
+					})}
 				</div>
 			</div>
 		</BaseCard>    
